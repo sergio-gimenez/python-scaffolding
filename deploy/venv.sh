01af8df4a17fbce567a7df9ab76cff1ab40207aa
Giménez, Sergio <sergio.gimenez@i2cat.net>
2022-11-14 17:16:57 +0100

    Initial commit

---
+++ deploy/venv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+current=$PWD
+
+curr_file_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+curr_root_path="$(dirname "${curr_file_path}")"
+venv_path="${curr_root_path}/.venv"
+cd ${curr_root_path}
+
+
+REQUIRED_PKG="virtualenv"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+
+if [[ ! -f ${venv_path} ]]; then
+    echo "Creating virtualenv folder"
+    virtualenv -p /usr/bin/python3.8 ${venv_path}
+fi
+
+echo """
+Start working on the virtualenv folder with:
+
+cd ${curr_root_path}
+source ${venv_path}/bin/activate
+
+When finished, type "deactivate" or remove the .venv folder
+"""
+
+cd $current
---
+++ logger.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+from colorlog import ColoredFormatter
+
+
+APP_LOGGER_NAME = 'IntentBasedAPI'
+COLORED_FORMATERR = "%(log_color)s%(levelname)s%(reset)s | [%(log_color)s%(name)s%(reset)s:%(log_color)s%(lineno)d%(reset)s] %(log_color)s%(message)s%(reset)s"
+FILE_FORMATTER = "[%(asctime)s] {%(name)s: %(lineno)d} %(levelname)s - %(message)s"
+
+
+def setup_logger(logger_name=APP_LOGGER_NAME, is_debug=True, file_name=None):
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG if is_debug else logging.INFO)
+
+    colored_formatter = ColoredFormatter(COLORED_FORMATERR)
+    file_formatter = logging.Formatter(FILE_FORMATTER)
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(colored_formatter)
+    logger.handlers.clear()
+    logger.addHandler(sh)
+
+    if file_name:
+        fh = logging.FileHandler(file_name)
+        fh.setFormatter(file_formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+
+def get_logger(module_name):
+    return logging.getLogger(APP_LOGGER_NAME).getChild(module_name)
